0ac81cb0c52b23136c90968538cbec79f60f8f6b
kenchan0226 <kenchanhp@gmail.com>
2021-08-26 16:51:56 +0800

    Implemented training and testing for talk2car.

--- train_task.py
+++ train_task.py
@@ -298,7 +298,7 @@ def main():
                     save(save_path, logger, epoch_id, model, optimizer, scheduler,
                          global_step, tb_logger, default_gpu, max_score, is_best=True)
 
-        logger.info('max validation score: {}'.format(max_score))
+        logger.info('max validation score: {}'.format(max_score * 100))
 
         save(save_path, logger, epoch_id, model, optimizer, scheduler, global_step, tb_logger, default_gpu, max_score)
 
