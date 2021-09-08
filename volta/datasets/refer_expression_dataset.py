675035a04f5a1bf5fc8392cc6c884d79e9d861f3
kenchan0226 <kenchanhp@gmail.com>
2021-09-08 14:17:51 +0800

    Implemented training and testing for talk2car.

--- volta/datasets/refer_expression_dataset.py
+++ volta/datasets/refer_expression_dataset.py
@@ -488,7 +488,7 @@ class ReferExpressionSequenceLabelDataset(ReferExpressionDataset):
         self.sequence_label_to_id = defaultdict(int)  # the default value is 0
         self.sequence_label_to_id["PROPN"] = 1
         self.sequence_label_to_id["NOUN"] = 1
-        self.sequence_label_to_id["ADJ"] = 1
+        #self.sequence_label_to_id["ADJ"] = 1
         #self.sequence_label_to_id["DET"] = 1
         #self.sequence_label_to_id["ADP"] = 1
         # PROPN, NOUN, ADJ, ADV, DET, ADP
