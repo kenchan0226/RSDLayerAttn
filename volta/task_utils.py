ce98d4f8c5f98b3336f82c539b9475f81c728607
kenchan0226 <kenchanhp@gmail.com>
2021-09-16 15:01:11 +0800

    Implemented training and testing for talk2car.

--- volta/task_utils.py
+++ volta/task_utils.py
@@ -1026,7 +1026,7 @@ def EvaluatingModel(config, task_cfg, device, task_id, batch, model, dataloader,
                     "id": question_id[i].item(),
                     "target": select_idx[i].item(),
                     "IOU": select_target[i].item(),
-                    "v_seq_len": image_mask[i].sum(),
+                    "v_seq_len": sum(image_mask[i]),
                     "layer_attn_scores": layer_attn_scores[i],
                 }
             )
